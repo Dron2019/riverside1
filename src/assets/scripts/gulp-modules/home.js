f4331b61ec9cc0a55c41d39535e0ddcde1132860
Dron2019 <mr.andrey@bigmir.net>
2020-06-11 16:30:44 +0300

    tabs functionall add

--- src/assets/scripts/gulp-modules/home.js
+++ src/assets/scripts/gulp-modules/home.js
@@ -160,3 +160,18 @@ var gallerySlider = new CanvasSlideshow({
     interactionEvent: 'click', // 'click', 'hover', 'both' 
 });
 /**Gallery distortion slider END*/
+
+/**TABS */
+let tabNavList = document.querySelectorAll('.forms .tab-head-item');
+let tabContainer = document.querySelector('.forms.tabs');
+tabNavList.forEach(el => {
+        el.addEventListener('click', function(evt) {
+            let dataLink = el.dataset.link;
+            tabContainer.querySelector('.tab-head-item.active').classList.remove('active');
+            el.classList.add('active');
+
+            tabContainer.querySelector(`.tab-body  .active`).classList.remove('active');
+            tabContainer.querySelector(`${dataLink}`).classList.add('active');
+        });
+    })
+    /**TABS END */
