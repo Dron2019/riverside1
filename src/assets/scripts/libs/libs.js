bb08747a0e6b26589fa525b7a9d11a6a3600eacb
Dron2019 <mr.andrey@bigmir.net>
2020-07-16 15:57:27 +0300

    tablet menu, about page start

--- src/assets/scripts/libs/libs.js
+++ src/assets/scripts/libs/libs.js
@@ -2,4 +2,6 @@
 @@include('../libs/jquery/jquery-3.5.0.min.js')
 @@include('../libs/magnific-popup/dist/jquery.magnific-popup.js')
 @@include('../libs/masked-input/masked-input.min.js')
+@@include('../libs/norm_wheel/norm_wheel.js')
+@@include('../../../../node_modules/bezier-easing/dist/bezier-easing.min.js')
 /* beautify preserve:end */
