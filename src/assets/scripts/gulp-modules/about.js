c7e7da463f56f2fdf335ad227601af415c65d7d0
Dron2019 <mr.andrey@bigmir.net>
2020-07-20 17:28:33 +0300

    developer page

---
+++ src/assets/scripts/gulp-modules/about.js
@@ -0,0 +1,9 @@
+/* beautify preserve:start */
+@@include('../libs/scrollme/jquery.scrollme.min.js')
+/* beautify preserve:end*/
+
+if (window.screen.width<576) {
+document.querySelectorAll('.about-text-block-right.scrollme.animateme').forEach(el=>{
+el.dataset.translatex = `-1000`;
+})
+}
