ee81b1c7a51aacf5ab2f3b9c2f3e547a4eea50ab
Dron2019 <mr.andrey@bigmir.net>
2020-07-30 18:09:33 +0300

    animation continue

--- src/assets/scripts/gulp-modules/about.js
+++ src/assets/scripts/gulp-modules/about.js
@@ -1,29 +1,20 @@
-/* beautify preserve:start */
-
-/* beautify preserve:end*/
-
-// if (window.screen.width<576) {
-// document.querySelectorAll('.about-text-block-right.scrollme.animateme').forEach(el=>{
-// el.dataset.translatex = `-1000`;
-// })
-// }
-
 let elementsAnimFunctions = {
     'about-text-block': {
-        func:function() {gsap.from(document.querySelector('.about-text-block'),{delay:1,opacity:0})},
-        defaultValues:{},
+        func: function() { gsap.from(document.querySelector('.about-text-block'), { delay: 1, opacity: 0 }) },
+        defaultValues: {},
     },
-    'about-separator-block__text':{
-        function() {gsap.from(document.querySelector('.about-separator-block__text'),{delay:1,opacity:0.5,y:'100%'})},
-        defaultValues:{},
+    'about-separator-block__text': {
+        function() { gsap.from(document.querySelector('.about-separator-block__text'), { delay: 1, opacity: 0.5, y: '100%' }) },
+        defaultValues: {},
     },
-
-
 };
+
+const ease_1 = BezierEasing(0, .7, 0, 1);
 gsap.defaults({
     ease: "power2.in",
-    duration: 1
-  });
+    duration: 1,
+    ease: ease_1
+});
 
 var options = {
     threshold: 0.1,
@@ -36,7 +27,7 @@ var callback = function(entries, observer) {
     if (entries[0].isIntersecting) {
         // console.log('sss');
         // elementsAnimFunctions[entries[0].target.classList[0]]['func']();
-        gsap.from(entries[0].target, {scale:1.1, opacity:0.9})
+        gsap.from(entries[0].target, { scale: 1.1, opacity: 0.9 })
         observer.unobserve(entries[0].target);
     }
 };
@@ -47,38 +38,50 @@ var observer = new IntersectionObserver(callback, options);
 //     observer.observe(el);
 // })
 // observer.observe(document.querySelector("#about > div.page__content > section:nth-child(3) > div"));
+
 gsap.registerPlugin(ScrollTrigger);
-
 ScrollTrigger.defaults({
-  markers: {startColor: "red", endColor: "red", fontSize: "18px", fontWeight: "bold", indent: 20},
-  onEnter: self=> {
-    // gsap.from(self.trigger, {y:'50%'});
+    markers: { startColor: "red", endColor: "red", fontSize: "18px", fontWeight: "bold", indent: 20 },
+    onUpdate: self => {
+        console.log(self.progress.toFixed(3));
+        if (+self.progress.toFixed(3) > 0.5) {
+            let tl = gsap.timeline();
+            tl.from(self.trigger, { y: '100%', duration: 1, opacity: 0 })
+                .from(self.trigger.querySelector('.about-text-block__main-subtitle'), { y: '100%', duration: 0.4 }, '<.2')
+                .from(self.trigger.querySelector('.about-text-block .text'), { y: '100%', duration: 0.5 }, '<.2');
+            self.kill();
+        }
+    },
+    onEnter: self => {
+        // gsap.from(self.trigger, {y:'50%'});
 
-    let tl =gsap.timeline();
-  // tl.fromTo(title, { x:'50vw', skewX:20, duration:1}, { x:-100, skewX:-20, duration:2})
-  tl.from(self.trigger, {y:'100%',duration:0.8})
-    .from(self.trigger.querySelector('.about-text-block__main-subtitle'), {y:'100%',duration:0.4},'<.2')
-    .from(self.trigger.querySelector('.about-text-block .text'), {y:'100%',duration:0.5},'<.2')
-    self.kill();
-  }
+
+        // tl.fromTo(title, { x:'50vw', skewX:20, duration:1}, { x:-100, skewX:-20, duration:2})
+        console.log(self.getVelocity);
+        // tl.from(self.trigger, { y: '100%', duration: 1, opacity: 0 })
+        //     .from(self.trigger.querySelector('.about-text-block__main-subtitle'), { y: '100%', duration: 0.4 }, '<.2')
+        //     .from(self.trigger.querySelector('.about-text-block .text'), { y: '100%', duration: 0.5 }, '<.2')
+        // self.kill();
+    }
 });
 let triggerSelectors = [
-  ".about-section-with-bg.first .about-text-block",
-  ".about-separator-block__text",
-  ".about-text-block.about-text-block-right.scrollme.animateme",
-  "#about > div.page__content > section:nth-child(5) > div",
-  "#about > div.page__content > section:nth-child(6) > div",
-  "#about > div.page__content > section:nth-child(7) > div",
-  "#about > div.page__content > section:nth-child(8) > div",
+    ".about-section-with-bg.first .about-text-block",
+    ".about-separator-block__text",
+    ".about-text-block.about-text-block-right.scrollme.animateme",
+    "#about > div.page__content > section:nth-child(5) > div",
+    "#about > div.page__content > section:nth-child(6) > div",
+    "#about > div.page__content > section:nth-child(7) > div",
+    "#about > div.page__content > section:nth-child(8) > div",
 ];
 let sections = document.querySelectorAll('.about-section-with-bg');
 
 
 
-triggerSelectors.forEach(selector=>{
-  ScrollTrigger.create({
-    trigger: selector,
-    start: "top bottom",
-    end: "bottom bottom",
-  });
+
+triggerSelectors.forEach(selector => {
+    ScrollTrigger.create({
+        trigger: selector,
+        start: "top bottom",
+        end: "bottom bottom",
+    });
 })
